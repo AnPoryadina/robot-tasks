c507b917f8cf69a09186d51475855874e21304eb
shibaeva-543 <Shibaeva_a@mail.ru>
2016-09-27 11:50:54 +0300

    Project skeleton

--- task_11.py
+++ task_11.py
@@ -6,10 +6,11 @@ from pyrob.api import *
 @task
 def task_8_4():
     while not wall_is_on_the_right():
-        if wall_is_above() and wall_is_beneath():
+       if wall_is_above() and wall_is_beneath():
             fill_cell()
-        move_right()
+       move_right()
     if wall_is_above() and wall_is_beneath():
-        fill_cell()
+       fill_cell()
+
 if __name__ == '__main__':
     run_tasks()
--- task_16.py
+++ task_16.py
@@ -5,15 +5,5 @@ from pyrob.api import *
 
 @task
 def task_8_22():
-    while wall_is_beneath() or wall_is_above() or wall_is_on_the_right() or wall_is_on_the_left():
-        if not wall_is_above():
-            move_up()
-        if not wall_is_on_the_left():
-            mo
-if __name__ == '__main__':
-    run_tasks()
-
-
-
-if __name__ == '__main__':
-    run_tasks()
+    while wall_is_above() or wall_is_on_the_left() or wall_is_beneath() or wall_is_on_the_right():
+        if not wall_is_above:if __name__ == '__main__':    run_tasks()
